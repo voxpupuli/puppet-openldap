78902503a3bc4923af2904a83b0ebab05beba963
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-01-10 16:18:08 +0100

    Add preseed on Debian

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,6 +1,23 @@
 # See README.md for details.
 class openldap::server::install {
+
+  if $::osfamily == 'Debian' {
+    file { '/var/cache/debconf/slapd.preseed':
+      ensure  => present,
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      content => template('openldap/preseed.erb'),
+    }
+  }
+
+  $responsefile = $::osfamily ? {
+    Debian => '/var/cache/debconf/slapd.preseed',
+    RedHat => undef,
+  }
+
   package { $::openldap::server::package:
-    ensure => present,
+    ensure       => present,
+    responsefile => $responsefile,
   }
 }
