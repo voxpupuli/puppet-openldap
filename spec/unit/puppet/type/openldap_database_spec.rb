efe22e5f99a7618a5d30142a07166e94e833fc47
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-02-06 08:54:08 +0100

    Update openldap_database type unit tests

--- spec/unit/puppet/type/openldap_database_spec.rb
+++ spec/unit/puppet/type/openldap_database_spec.rb
@@ -3,19 +3,79 @@
 require 'spec_helper'
 
 describe Puppet::Type.type(:openldap_database) do
-  describe 'without parameters' do
-    resource = Puppet::Type.type(:openldap_database).new :title => 'dc=example,dc=com'
-    it 'should have :suffix be its namevar' do
-      resource[:suffix].should == 'dc=example,dc=com'
+  before do
+    @provider_class = described_class.provide(:simple) { mk_resource_methods }
+    @provider_class.stubs(:suitable?).returns true
+    described_class.stubs(:defaultprovider).returns @provider_class
+  end
+
+  describe "namevar validation" do
+    it "should have :suffix as its namevar" do
+      described_class.key_attributes.should == [:suffix]
     end
-    it 'should have :backend be set to hdb' do
-      resource[:backend].should == :hdb
+    it "should not invalid suffixes" do
+      pending('must implement validation') do
+        expect { described_class.new(:name => 'foo bar') }.to raise_error(Puppet::Error, /Invalid value/)
+      end
+      expect { described_class.new(:name => 'cn=admin,dc=example,dc=com') }.to raise_error(Puppet::Error, /Invalid value/)
+      expect { described_class.new(:name => 'dc=example, dc=com') }.to raise_error(Puppet::Error, /Invalid value/)
     end
-    it 'should have :rootdn be set to nil' do
-      resource[:rootdn].should == nil
-    end
-    it 'should have :rootpw be set to nil' do
-      resource[:rootpw].should == nil
+    it "should allow valid suffix" do
+      expect { described_class.new(:name => 'dc=example,dc=com') }.to_not raise_error
     end
   end
+
+  describe "when validating attributes" do
+    [:suffix, :provider].each do |param|
+      it "should have a #{param} parameter" do
+        described_class.attrtype(param).should == :param
+      end
+    end
+
+    [:backend, :directory].each do |property|
+      it "should have a #{property} property" do
+        described_class.attrtype(property).should == :property
+      end
+    end
+  end
+
+  describe "when validating values" do
+
+    describe "ensure" do
+      it "should support present as a value for ensure" do
+        expect { described_class.new(:name => 'foo', :ensure => :present) }.to_not raise_error
+      end
+      it "should support absent as a value for ensure" do
+        expect { described_class.new(:name => 'foo', :ensure => :absent) }.to_not raise_error
+      end
+      it "should not support other values" do
+        expect { described_class.new(:name => 'foo', :ensure => :foo) }.to raise_error(Puppet::Error, /Invalid value/)
+      end
+    end
+
+    describe "backend" do
+      it "should support bdb as a value for backend" do
+        expect { described_class.new(:name => 'foo', :backend => 'bdb') }.to_not raise_error
+      end
+      it "should support hdb as a value for backend" do
+        expect { described_class.new(:name => 'foo', :backend => 'hdb') }.to_not raise_error
+      end
+      it "should not support other values" do
+        expect { described_class.new(:name => 'foo', :backend => 'bar') }.to raise_error(Puppet::Error, /Invalid value/)
+      end
+    end
+
+    describe "directory" do
+      it "should support an absolute path as a value for directory" do
+        expect { described_class.new(:name => 'foo', :directory => '/bar/baz') }.to_not raise_error
+      end
+      it "should not support other values" do
+        pending('Must implement validation') do
+          expect { described_class.new(:name => 'foo', :directory => 'bar/baz') }.to raise_error(Puppet::Error, /kjsflkjdsflk/)
+        end
+      end
+    end
+
+  end
+
 end
