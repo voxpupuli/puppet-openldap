b1a07d4d6bca881215a0163a7d8317828713653b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-05-12 14:53:57 +0200

    Add acceptance test for openldap::server::access

---
+++ spec/acceptance/openldap__server__access_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper_acceptance'
+
+describe 'openldap::server::access define' do
+  describe 'Add an ACL' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+        class { 'openldap::server':
+          databases => {
+            'dc=foo,dc=example,dc=org' => {
+              directory => '/var/lib/ldap/foo',
+            },
+          },
+        }
+        openldap::server::access { 'to attrs=userPassword,shadowLastChange by dn="cn=admin,dc=foo,dc=example,dc=org" on dc=foo,dc=example,dc=org':
+	  access => 'write',
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+    end
+  end
+end
+
