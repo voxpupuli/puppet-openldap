0d80c690a89bef394cc9f149a5bedce6a88cf58c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-06-15 20:14:52 +0200

    Don't set directory when removing dc=my-domain,dc=com database

--- manifests/server/slapdconf.pp
+++ manifests/server/slapdconf.pp
@@ -41,7 +41,6 @@ class openldap::server::slapdconf {
     and !member(keys($::openldap::server::databases), 'dc=my-domain,dc=com') {
     openldap::server::database { 'dc=my-domain,dc=com':
       ensure    => absent,
-      directory => '/var/lib/ldap',
     }
   }
 
