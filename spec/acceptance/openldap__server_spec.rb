f7a1bad77be76b1385c5ab30e06eaf543c643f08
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-21 18:14:58 +0200

    Update acceptance tests

---
+++ spec/acceptance/openldap__server_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper_acceptance'
+
+describe 'openldap::server' do
+
+  context 'with defaults' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'openldap::server': }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(389) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(636) do
+      it { should_not be_listening }
+    end
+
+    it 'can connect with ldapsearch' do
+      ldapsearch('-LLL -x -b dc=example,dc=com') do |r|
+        expect(r.stdout).to match(/dn: dc=example,dc=com/)
+      end
+    end
+  end
+
+  context 'when adding certificates' do
+    it 'with cert, key and ca' do
+      pp = <<-EOS
+        class { 'openldap::server':
+          ssl_key   => "/etc/ldap/ssl/${::fqdn}.key",
+          ssl_cert  => "/etc/ldap/ssl/${::fqdn}.crt",
+          ssl_ca    => '/etc/ldap/ssl/ca.pem',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(389) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(636) do
+      it { should_not be_listening }
+    end
+
+    it 'can connect with ldapsearch using StartTLS' do
+      pending
+      ldapsearch('-LLL -x -b dc=example,dc=com -ZZ') do |r|
+        expect(r.stdout).to match(/dn: dc=example,dc=com/)
+      end
+    end
+  end
+
+  context 'when enabling ldaps' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'openldap::server':
+          ldaps_ifs => ['/'],
+          ssl_key   => "/etc/ldap/ssl/${::fqdn}.key",
+          ssl_cert  => "/etc/ldap/ssl/${::fqdn}.crt",
+          ssl_ca    => '/etc/ldap/ssl/ca.pem',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(389) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(636) do
+      it { is_expected.to be_listening }
+    end
+
+    it 'can connect with ldapsearch using ldaps:///' do
+      pending
+      ldapsearch('-LLL -x -b dc=example,dc=com -H ldaps:///') do |r|
+        expect(r.stdout).to match(/dn: dc=example,dc=com/)
+      end
+    end
+  end
+
+  context 'when updating suffix' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'openldap::server':
+          suffix => 'dc=foo,dc=com',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(389) do
+      it { is_expected.to be_listening }
+    end
+
+    describe port(636) do
+      it { should_not be_listening }
+    end
+
+    it 'can connect with ldapsearch' do
+      pending
+      ldapsearch('-LLL -x -b dc=foo,dc=com') do |r|
+        expect(r.stdout).to match(/dn: dc=foo,dc=com/)
+      end
+    end
+  end
+
+end
