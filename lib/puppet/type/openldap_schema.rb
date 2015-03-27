09359441cd6d67c15a432fc5c84a1816f470912c
Max Maton <max@maton.info>
2015-03-27 01:58:36 +0000

    Fixed issue with schemas overwriting acls, refactored

--- lib/puppet/type/openldap_schema.rb
+++ lib/puppet/type/openldap_schema.rb
@@ -11,4 +11,12 @@ Puppet::Type.newtype(:openldap_schema) do
     desc "The location to the schema file."
   end
 
+  def pathWithDefault
+      if self[:path]
+          self[:path]
+      else
+          "/etc/ldap/schema/" + self[:name] + ".schema"
+      end
+  end
+
 end
