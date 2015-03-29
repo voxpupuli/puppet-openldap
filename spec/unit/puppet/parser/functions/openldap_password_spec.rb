4b2f0b2741eda4345810c5705c1bfe65d5e0498e
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-29 08:58:32 +0200

    Use ruby to generate idempotent SSHA password (Fixes #35)

--- spec/unit/puppet/parser/functions/openldap_password_spec.rb
+++ spec/unit/puppet/parser/functions/openldap_password_spec.rb
@@ -3,33 +3,36 @@ require 'spec_helper'
 describe Puppet::Parser::Functions.function(:openldap_password) do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
-  it 'should exist' do
-    expect(Puppet::Parser::Functions.function('openldap_password')).to eq('function_openldap_password')
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      before :each do
+        facts.each do |k, v|
+          scope.stubs(:lookupvar).with("::#{k}").returns(v)
+          scope.stubs(:lookupvar).with(k).returns(v)
+        end
+      end
 
-  context 'when given a wrong number of arguments' do
-    it 'should fail' do
-      expect {
-        scope.function_openldap_password([])
-      }.to raise_error Puppet::ParseError, /Wrong number of arguments given/
-    end
-  end
+      it 'should exist' do
+        expect(
+          Puppet::Parser::Functions.function('openldap_password')
+        ).to eq('function_openldap_password')
+      end
 
-  context 'when given only a secret' do
-    it 'should execute slappasswd on it' do
-      Puppet::Util::Execution.stubs(:execute).with([
-        'slappasswd', '-s', 'foo'
-      ]).returns("{SSHA}kKSBVuPOwlHp5HfcR3LBKyB7smTnbq9Y\n")
-      expect(scope.function_openldap_password(['foo'])).to eq('{SSHA}kKSBVuPOwlHp5HfcR3LBKyB7smTnbq9Y')
-    end
-  end
+      context 'when given a wrong number of arguments' do
+        it 'should fail' do
+          expect {
+            scope.function_openldap_password([])
+          }.to raise_error Puppet::ParseError, /Wrong number of arguments given/
+        end
+      end
 
-  context 'when given a secret and a scheme' do
-    it 'should execute slappasswd on them' do
-      Puppet::Util::Execution.stubs(:execute).with([
-        'slappasswd', '-s', 'foo', '-h', '{MD5}'
-      ]).returns("{MD5}rL0Y20zC+Fzt72VPzMSk2A==\n")
-      expect(scope.function_openldap_password(['foo', '{MD5}'])).to eq('{MD5}rL0Y20zC+Fzt72VPzMSk2A==')
+      context 'when given only a secret' do
+        it 'should return the SSHA of the password with sha1("foo.example.com") as salt' do
+          expect(
+            scope.function_openldap_password(['secret'])
+          ).to eq('{SSHA}jZdUkbyDYvmpSKg0x/k879g+RY7EHbws5g==')
+        end
+      end
     end
   end
 end
