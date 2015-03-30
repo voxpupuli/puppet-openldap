ca60a801179b9ded3405cd053567593b07fca96e
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-30 09:32:55 +0200

    Try do set selinux on permissive only if enabled

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -17,7 +17,7 @@ hosts.each do |host|
     end
     install_package host, 'libaugeas-ruby'
   when 'RedHat'
-    on host, 'setenforce 0'
+    on host, 'setenforce 0' if fact('selinux') == 'true'
     install_package host, 'gcc'
     install_package host, 'ruby-devel'
     install_package host, 'augeas-devel'
