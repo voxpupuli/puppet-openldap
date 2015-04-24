2a7626ea8f53e45df8919c557bd3d031bb722e46
Max Maton <max@maton.info>
2015-04-24 17:07:04 +0000

    Changed schema provider to convert schemas to ldif locally

--- lib/puppet/provider/openldap_schema/olc.rb
+++ lib/puppet/provider/openldap_schema/olc.rb
@@ -6,114 +6,83 @@ Puppet::Type.type(:openldap_schema).provide(:olc) do
 
   defaultfor :osfamily => :debian, :osfamily => :redhat
 
-  commands :slapcat => 'slapcat', :ldapadd => 'ldapadd', :slaptest => 'slaptest'
+  commands :slapcat => 'slapcat', :ldapadd => 'ldapadd'
 
   mk_resource_methods
 
-  @@openldap_schema_existing = []
-  @@openldap_schema_new = []
-  @@openldap_schema_create_count = 0
-  @@openldap_schema_max_count = 0
-
   def self.instances
-      schemas = []
-      slapcat('-H', 'ldap:///???(objectClass=olcSchemaConfig)', '-b', 'cn=config').split("\n\n").each do |paragraph|
-          paragraph.split("\n").each do |line|
-              if line =~ /^cn: \{/
-                  schemas.push line
-              end
-          end
+    schemas = []
+    slapcat('-H', 'ldap:///???(objectClass=olcSchemaConfig)', '-b', 'cn=config').split("\n\n").each do |paragraph|
+      paragraph.split("\n").each do |line|
+        if line =~ /^cn: \{/
+          schemas.push line
+        end
       end
-      names = schemas.map{ |entry| entry.match(/^cn: \{\d+\}(\S+)/)[1] }
-      names.map { |schema|
-          new(
-            :ensure => :present,
-            :name   => schema
-          )
-      }
+    end
+    names = schemas.map{ |entry| entry.match(/^cn: \{\d+\}(\S+)/)[1] }
+    names.map { |schema|
+      new(
+        :ensure => :present,
+        :name		=> schema
+      )
+    }
+        end
+
+  def self.schemaToLdif(schema, name)
+    ldif = [
+      "dn: cn=#{name},cn=schema,cn=config",
+      "objectClass: olcSchemaConfig",
+        "cn: #{name}",
+    ]
+    schema.split("\n").each do |line|
+      case line
+      when /^\s*#/									# Comments are ok
+        ldif.push(line)
+      when /^$/										# Replace empty lines with comment
+        ldif.push("#")
+      when /^objectidentifier(.*)$/i				# Rewrite tags
+        ldif.push("olcObjectIdentifier:#{$1}")
+      when /^attributetype(.*)$/i
+        ldif.push("olcAttributeTypes:#{$1}")
+      when /^objectclass(.*)$/i
+        ldif.push("olcObjectClasses:#{$1}")
+      when /^\s+(.*)/								# Rewrite continuation whitespace
+        ldif.push("  #{$1}")						# One space to indicate continuation, plus one for spacing between words
+      end
+    end
+    ldif.join("\n")
   end
 
   def self.prefetch(resources)
-    # Match installed schemas with requested schemas to find the paths to them
-    installed = instances.map{ |i| i.name }
-    for schema in installed do
-        if match = resources.values.find{ |resource| resource[:path] =~ /#{Regexp.escape(schema)}\.schema$/ }
-            @@openldap_schema_existing.push(match)
-        else
-            warning("Missing schema '#{schema}', might cause insertion issues")
-        end
-    end
-
-    mods = instances
-    
+    existing = instances
     resources.keys.each do |name|
-      if provider = mods.find{ |mod| mod.name == name }
+      if provider = existing.find { |r| r.name == name }
         resources[name].provider = provider
-      else
-        @@openldap_schema_max_count += 1
       end
     end
   end
 
   def create
-    # Order new resources by priority
-    @@openldap_schema_new.push(resource)
-
-    # Only insert in the last requested resource
-    @@openldap_schema_create_count += 1
-    unless @@openldap_schema_create_count == @@openldap_schema_max_count
-        return
-    end
 
     t = Tempfile.new('openldap_schemas_ldif')
-    t2 = Tempfile.new('openldap_schemas_includes')
-    
-    for resource in @@openldap_schema_existing do
-        t2 << "include #{resource[:path]}\n"
-    end
-    for resource in @@openldap_schema_new do
-        t2 << "include #{resource[:path]}\n"
-    end
-
-    t2.close
 
     begin
-      Dir.mktmpdir{|ldif_dir|
-        slaptest('-f', t2.path, '-F', ldif_dir)
-        ldif = slapcat('-n0', '-F', ldif_dir).split("\n\n")
-
-        filtered = []
-        
-        ldif = ldif.each do |paragraph|
-            # Make sure we don't add other configuration files other than the schemas or we lose acls
-            description = paragraph.split("\n")[0]
-            if match = description.match(/^dn: cn=\{\d+\}([^,]+),cn=schema,cn=config/)
-                name = match[1]
-                unless @@openldap_schema_new.any?{ |resource| resource.name == name }
-                    next
-                end
-                paragraph.split("\n").each do |line|
-                    # Convert between slapcat and ldapadd (slapadd needs a stopped slapd)
-                    unless line =~ /^(creatorsName|createTimestamp|modifiersName|modifyTimestamp|structuralObjectClass|entryUUID|entryCSN): /
-                        filtered.push(line)
-                    end
-                end
-                filtered.push("")
-            end
-        end
-        filtered = filtered.join("\n")
-        t.write filtered
-        t.close
-      }
+      schema = IO.read resource[:path]
+      t << self.class.schemaToLdif(schema, resource[:name])
+      t.close
 
       ldapadd('-cQY', 'EXTERNAL', '-H', 'ldapi:///', '-f', t.path)
     rescue Exception => e
-      raise Puppet::Error, "Config: #{IO.read t2.path}\n LDIF content:\n#{IO.read t.path}\nError message: #{e.message}"
+      raise Puppet::Error, "LDIF content:\n#{IO.read t.path}\nError message: #{e.message}"
     end
     @property_hash[:ensure] = :present
-  end
+    end
 
   def exists?
     @property_hash[:ensure] == :present
+    end
+
+  def destroy
+    raise Puppet::Error, "Removing schemas is not supported by this provider. Slapd needs to be stopped and the schema must be removed manually."
   end
 end
