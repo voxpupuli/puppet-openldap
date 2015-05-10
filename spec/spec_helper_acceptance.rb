2cd2d62d96e2d2acfb7efbc4cf7106e87616e693
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-05-10 08:59:41 +0200

    Don't generate certificate for acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -28,7 +28,6 @@ hosts.each do |host|
     puts 'Sorry, this osfamily is not supported.'
     exit
   end
-  on host, 'puppet cert generate $(facter fqdn)'
   install_package host, 'net-tools'
 end
 
@@ -43,51 +42,6 @@ RSpec.configure do |c|
   c.before :suite do
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'openldap')
-
-    # Set up Certificates
-    pp = <<-EOS
-      $ssldir = '/var/lib/puppet/ssl'
-      file { '/etc/ldap':
-        ensure => directory,
-      }
-      file { '/etc/ldap/ssl':
-        ensure => directory,
-      }
-      if $::osfamily == 'Debian' {
-        # OpenLDAP is linked towards GnuTLS on Debian so we have to convert the key
-        package { 'gnutls-bin':
-          ensure => present,
-        }
-        ->
-        exec { "certtool -k < ${ssldir}/private_keys/${::fqdn}.pem > /etc/ldap/ssl/${::fqdn}.key":
-          creates => "/etc/ldap/ssl/${::fqdn}.key",
-          path    => $::path,
-          require => File['/etc/ldap/ssl'],
-          before  => File["/etc/ldap/ssl/${::fqdn}.key"],
-        }
-      } else {
-        File <| title == "/etc/ldap/ssl/${::fqdn}.key" |> {
-          source => "${ssldir}/private_keys/${::fqdn}.pem",
-        }
-      }
-      file { "/etc/ldap/ssl/${::fqdn}.key":
-        ensure  => file,
-        mode    => '0644',
-      }
-      file { "/etc/ldap/ssl/${::fqdn}.crt":
-        ensure  => file,
-        mode    => '0644',
-        source  => "${ssldir}/certs/${::fqdn}.pem",
-      }
-      file { '/etc/ldap/ssl/ca.pem':
-        ensure  => file,
-        mode    => '0644',
-        source  => "${ssldir}/certs/ca.pem",
-      }
-    EOS
-
-    apply_manifest_on(hosts, pp, :catch_failures => false)
-
     hosts.each do |host|
       on host, puppet('module','install','herculesteam-augeasproviders_core'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','herculesteam-augeasproviders_shellvar'), { :acceptable_exit_codes => [0,1] }
