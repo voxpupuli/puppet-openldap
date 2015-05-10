6835a73da146838deeaf8e40ee66810b1fafc063
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-05-10 09:00:15 +0200

    Update spec/rspec_puppet_facts.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -33,7 +33,8 @@ end
 
 RSpec.configure do |c|
   # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_name = module_root.split('-').last
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -41,7 +42,7 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'openldap')
+    puppet_module_install(:source => module_root, :module_name => module_name)
     hosts.each do |host|
       on host, puppet('module','install','herculesteam-augeasproviders_core'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','herculesteam-augeasproviders_shellvar'), { :acceptable_exit_codes => [0,1] }
