f20cd606776e8fa08da59340090810ba8be96933
Baptiste Grenier <bgrenier@gnubila.fr>
2015-10-20 17:19:17 +0200

    openldap::server::slapdconf spec: test behaviour of ssl configuration on rhel with a moznss db

--- spec/classes/openldap_server_slapdconf_spec.rb
+++ spec/classes/openldap_server_slapdconf_spec.rb
@@ -16,6 +16,47 @@ describe 'openldap::server::slapdconf' do
         it { is_expected.to contain_class('openldap::server::slapdconf') }
         it { is_expected.to contain_openldap__server__database('dc=my-domain,dc=com').with({:ensure => :absent,})}
       end
+
+      # On rhel, openldap is linked against moz nss:
+      # ssl_ca parameter should contain the path to the moz nss database
+      # ssl_cert parameter should contain the name of the certificate stored into the moz nss database
+      # ssl_key can be omitted/is not used
+      context 'with ssl_cert and ssl_ca set but not ssl_key' do
+        let :pre_condition do
+          "class {'openldap::server': ssl_cert => 'my-domain.com', ssl_ca => '/etc/openldap/certs'}"
+        end
+        case facts[:osfamily]
+        when 'Debian'
+          it { is_expected.to contain_class('openldap::server::slapdconf') }
+          it { expect { is_expected.to compile }.to raise_error(/You must specify a ssl_key/) }
+        when 'RedHat'
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('openldap::server::slapdconf') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCertificateFile') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCACertificateFile') }
+          it { is_expected.not_to contain_openldap__globalconf( 'TLSCertificateKeyFile') }
+        end
+      end
+
+      context 'with ssl_cert, ssl_key and ssl_ca set' do
+        let :pre_condition do
+          "class {'openldap::server': ssl_cert => '/etc/openldap/certs/fqdn.tld.crt', ssl_key => '/etc/openldap/certs/fqdn.tld.key', ssl_ca => '/etc/openldap/certs/ca.crt'}"
+        end
+        case facts[:osfamily]
+        when 'Debian'
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('openldap::server::slapdconf') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCertificateFile') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCACertificateFile') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCertificateKeyFile') }
+        when 'RedHat'
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('openldap::server::slapdconf') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCertificateFile') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCACertificateFile') }
+          it { is_expected.to contain_openldap__globalconf( 'TLSCertificateKeyFile') }
+        end
+      end
     end
   end
 end
