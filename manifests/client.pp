2de7e497974c87201d2cee30fa7fc2cf640f7685
matt.cover <matt.cover@highwinds.com>
2015-10-30 12:30:00 -0700

    Add serveral params for ldap.conf to openldap::client.

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,14 +1,34 @@
 # See README.md for details.
 class openldap::client(
-  $package    = $openldap::params::client_package,
-  $file       = $openldap::params::client_conffile,
+  $package              = $openldap::params::client_package,
+  $file                 = $openldap::params::client_conffile,
 
   # Options
-  $base       = undef,
-  $uri        = undef,
+  $base                 = undef,
+  $bind_policy          = undef,
+  $ldap_version         = undef,
+  $scope                = undef,
+  $ssl                  = undef,
+  $suffix               = undef,
+  $uri                  = undef,
+
+  # NSS Options
+  $nss_base_group       = undef,
+  $nss_base_hosts       = undef,
+  $nss_base_passwd      = undef,
+  $nss_base_shadow      = undef,
+
+  # PAM Options
+  $pam_filter           = undef,
+  $pam_login_attribute  = undef,
+  $pam_member_attribute = undef,
+  $pam_password         = undef,
 
   # TLS Options
-  $tls_cacert = undef,
+  $tls_cacert           = undef,
+  $tls_cacertdir        = undef,
+  $tls_checkpeer        = undef,
+  $tls_reqcert          = undef,
 ) inherits ::openldap::params {
   anchor { 'openldap::client::begin': } ->
   class { '::openldap::client::install': } ->
--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -10,16 +10,97 @@ class openldap::client::config {
       changes => "set BASE ${::openldap::client::base}",
     }
   }
+  if $::openldap::client::bind_policy != undef {
+    augeas { 'ldap.conf+bind_policy':
+      changes  => "set BIND_POLICY ${::openldap::client::bind_policy}",
+    }
+  }
+  if $::openldap::client::ldap_version != undef {
+    augeas { 'ldap.conf+ldap_version':
+      changes  => "set LDAP_VERSION ${::openldap::client::ldap_version}",
+    }
+  }
+  if $::openldap::client::scope != undef {
+    augeas { 'ldap.conf+scope':
+      changes  => "set SCOPE ${::openldap::client::scope}",
+    }
+  }
+  if $::openldap::client::ssl != undef {
+    augeas { 'ldap.conf+ssl':
+      changes  => "set SSL ${::openldap::client::ssl}",
+    }
+  }
+  if $::openldap::client::suffix != undef {
+    augeas { 'ldap.conf+suffix':
+      changes  => "set SUFFIX ${::openldap::client::suffix}",
+    }
+  }
   if $::openldap::client::uri != undef {
     $_uri = join(flatten([$::openldap::client::uri]), ' ')
     augeas { 'ldap.conf+uri':
       changes => "set URI '${_uri}'",
     }
   }
+  if $::openldap::client::nss_base_group != undef {
+    augeas { 'ldap.conf+nss_base_group':
+      changes  => "set NSS_BASE_GROUP ${::openldap::client::nss_base_group}",
+    }
+  }
+  if $::openldap::client::nss_base_hosts != undef {
+    augeas { 'ldap.conf+nss_base_hosts':
+      changes  => "set NSS_BASE_HOSTS ${::openldap::client::nss_base_hosts}",
+    }
+  }
+  if $::openldap::client::nss_base_passwd != undef {
+    augeas { 'ldap.conf+nss_base_passwd':
+      changes  => "set NSS_BASE_PASSWD ${::openldap::client::nss_base_passwd}",
+    }
+  }
+  if $::openldap::client::nss_base_shadow != undef {
+    augeas { 'ldap.conf+nss_base_shadow':
+      changes  => "set NSS_BASE_SHADOW ${::openldap::client::nss_base_shadow}",
+    }
+  }
+  if $::openldap::client::pam_filter != undef {
+    augeas { 'ldap.conf+pam_filter':
+      changes  => "set PAM_FILTER ${::openldap::client::pam_filter}",
+    }
+  }
+  if $::openldap::client::pam_login_attribute != undef {
+    augeas { 'ldap.conf+pam_login_attribute':
+      changes  => "set PAM_LOGIN_ATTRIBUTE ${::openldap::client::pam_login_attribute}",
+    }
+  }
+  if $::openldap::client::pam_member_attribute != undef {
+    augeas { 'ldap.conf+pam_member_attribute':
+      changes  => "set PAM_MEMBER_ATTRIBUTE ${::openldap::client::pam_member_attribute}",
+    }
+  }
+  if $::openldap::client::pam_password != undef {
+    augeas { 'ldap.conf+pam_password':
+      changes  => "set PAM_PASSWORD ${::openldap::client::pam_password}",
+    }
+  }
+  if $::openldap::client::tls_checkpeer != undef {
+    augeas { 'ldap.conf+tls_checkpeer':
+      changes  => "set TLS_CHECKPEER ${::openldap::client::tls_checkpeer}",
+    }
+  }
   if $::openldap::client::tls_cacert != undef {
     validate_absolute_path($::openldap::client::tls_cacert)
     augeas { 'ldap.conf+tls_cacert':
       changes  => "set TLS_CACERT ${::openldap::client::tls_cacert}",
+    }
+  }
+  if $::openldap::client::tls_cacertdir != undef {
+    validate_absolute_path($::openldap::client::tls_cacertdir)
+    augeas { 'ldap.conf+tls_cacertdir':
+      changes  => "set TLS_CACERTDIR ${::openldap::client::tls_cacertdir}",
+    }
+  }
+  if $::openldap::client::tls_reqcert != undef {
+    augeas { 'ldap.conf+tls_reqcert':
+      changes  => "set TLS_REQCERT ${::openldap::client::tls_reqcert}",
     }
   }
 }
