49b2cf4697c54f2b0bc5c18b95917191a55fa5f0
Patrick Brideau <pbrideau@kronostechnologies.com>
2015-11-18 12:13:48 -0500

    Rebase openldap_access to allow array of "access" including "by" and "control" values
    Cont of rebase openldap_access

--- lib/puppet/provider/openldap_access/olc.rb
+++ lib/puppet/provider/openldap_access/olc.rb
@@ -25,19 +25,18 @@ Puppet::Type.
           suffix = line.split(' ')[1]
         when /^olcAccess: /
           position, what, bys = line.match(/^olcAccess:\s+\{(\d+)\}to\s+(\S+(?:\s+filter=\S+)?(?:\s+attrs=\S+)?)(\s+by\s+.*)+$/).captures
-          bys.split(' by ')[1..-1].each { |b|
-            by, access, control = b.strip.match(/^(\S+\"[^\"]+\"|\S+)\s+(\S+)(\s+\S+)?$/).captures
-            i << new(
-              :name     => "to #{what} by #{by} on #{suffix}",
-              :ensure   => :present,
-              :position => position,
-              :what     => what,
-              :by       => by,
-              :suffix   => suffix,
-              :access   => access,
-              :control  => control
-            )
+          access = []
+          bys.split(/(?= by .+)/).each { |b|
+            access << b.lstrip
           }
+          i << new(
+            :name     => "#{position} on #{suffix}",
+            :ensure   => :present,
+            :position => position,
+            :what     => what,
+            :access   => access,
+            :suffix   => suffix,
+          )
         end
       end
     end
@@ -49,16 +48,15 @@ Puppet::Type.
     accesses = instances
     resources.keys.each do |name|
       if provider = accesses.find{ |access|
-        access.what == resources[name][:what] &&
-          access.by == resources[name][:by] &&
-          access.suffix == resources[name][:suffix]
+        access.suffix == resources[name][:suffix] &&
+        access.position == resources[name][:position]
       }
         resources[name].provider = provider
       end
     end
   end
 
-  def getDn(suffix)
+  def self.getDn(suffix)
     if suffix == 'cn=frontend'
       return 'olcDatabase={-1}frontend,cn=config'
     elsif suffix == 'cn=config'
@@ -85,9 +83,16 @@ Puppet::Type.
   def create
     position = "{#{resource[:position]}}" if resource[:position]
     t = Tempfile.new('openldap_access')
-    t << "dn: #{getDn(resource[:suffix])}\n"
+    t << "dn: #{self.class.getDn(resource[:suffix])}\n"
     t << "add: olcAccess\n"
-    t << "olcAccess: #{position}to #{resource[:what]} by #{resource[:by]} #{resource[:access]}\n"
+    if resource[:position]
+      t << "olcAccess: {#{resource[:position]}}to #{resource[:what]}\n"
+    else
+      t << "olcAccess: to #{resource[:what]}\n"
+    end
+    resource[:access].each do |a|
+      t << "  #{a}\n"
+    end
     t.close
     Puppet.debug(IO.read t.path)
     begin
@@ -99,13 +104,30 @@ Puppet::Type.
 
   def destroy
     t = Tempfile.new('openldap_access')
-    t << "dn: #{getDn(@property_hash[:suffix])}\n"
+    t << "dn: #{self.class.getDn(@property_hash[:suffix])}\n"
     t << "changetype: modify\n"
     t << "delete: olcAccess\n"
     t << "olcAccess: {#{@property_hash[:position]}}\n"
     t.close
     Puppet.debug(IO.read t.path)
     slapdd('-b', 'cn=config', '-l', t.path)
+  end
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  def what=(value)
+    @property_flush[:what] = value
+  end
+
+  def suffix=(value)
+    @property_flush[:suffix] = value
+  end
+
+  def position=(value)
+    @property_flush[:position] = value
   end
 
   def access=(value)
@@ -123,7 +145,60 @@ Puppet::Type.
       ldapmodify(t.path)
     rescue Exception => e
       raise Puppet::Error, "LDIF content:\n#{IO.read t.path}\nError message: #{e.message}"
-    end
   end
+
+  def getCurrentOlcAccess(suffix)
+    i = []
+    slapcat(
+      '-H',
+      "ldap:///#{self.class.getDn(suffix)}???(olcAccess=*)"
+    ).split("\n\n").collect do |paragraph|
+      paragraph.gsub("\n ", '').split("\n").collect do |line|
+        case line
+        when /^olcAccess: /
+          position, content = line.match(/^olcAccess:\s+\{(\d+)\}(.*)$/).captures
+          i << {
+            :position => position,
+            :content => content,
+          }
+        end
+      end
+    end
+    return i
+  end
+
+  def flush
+    if not @property_flush.empty?
+      current_olcAccess = getCurrentOlcAccess(resource[:suffix])
+      t = Tempfile.new('openldap_access')
+      t << "dn: #{self.class.getDn(resource[:suffix])}\n"
+      t << "changetype: modify\n"
+      t << "replace: olcAccess\n"
+      if resource[:position]
+        position = resource[:position]
+      else
+        position = @property_hash[:position]
+      end
+      current_olcAccess.each do |olcAccess|
+        if olcAccess[:position].to_i == position.to_i
+          t << "olcAccess: {#{position}}to #{resource[:what]}\n"
+          resource[:access].each do |a|
+            t << "  #{a}\n"
+          end
+        else
+          t << "olcAccess: {#{olcAccess[:position]}}#{olcAccess[:content]}\n"
+        end
+      end
+      t.close
+      Puppet.debug(IO.read t.path)
+      begin
+        ldapmodify('-Y', 'EXTERNAL', '-H', 'ldapi:///', '-f', t.path)
+      rescue Exception => e
+        raise Puppet::Error, "LDIF content:\n#{IO.read t.path}\nError message: #{e.message}"
+      end
+    end
+    @property_hash = resource.to_hash
+  end
+
 
 end
--- lib/puppet/type/openldap_access.rb
+++ lib/puppet/type/openldap_access.rb
@@ -11,35 +11,22 @@ Puppet::Type.newtype(:openldap_access) do
     desc "The slapd.conf file"
   end
 
-  newparam(:what, :namevar => true) do
+  newproperty(:what) do
     desc "The entries and/or attributes to which the access applies"
   end
 
-  newparam(:by, :namevar => true) do
-    desc "To whom the access applies"
-  end
-
-  newparam(:suffix, :namevar => true) do
+  newproperty(:suffix) do
     desc "The suffix to which the access applies"
   end
 
   def self.title_patterns
     [
       [
-        /^(to\s+(\S+)\s+by\s+(.+)\s+on\s+(.+))$/,
+        /^((\S+)\s+on\s+(.+))$/,
         [
           [ :name, lambda{|x| x} ],
-          [ :what, lambda{|x| x} ],
-          [ :by, lambda{|x| x} ],
+          [ :position, lambda{|x| x} ],
           [ :suffix, lambda{|x| x} ],
-        ],
-      ],
-      [
-        /^(to\s+(\S+)\s+by\s+(.+))$/,
-        [
-          [ :name, lambda{|x| x} ],
-          [ :what, lambda{|x| x} ],
-          [ :by, lambda{|x| x} ],
         ],
       ],
       [
@@ -51,16 +38,12 @@ Puppet::Type.newtype(:openldap_access) do
     ]
   end
 
-  newparam(:position) do
+  newproperty(:position) do
     desc "Where to place the new entry"
   end
 
-  newproperty(:access) do
+  newproperty(:access, :array_matching => :all) do
     desc "Access rule."
-  end
-
-  newproperty(:control) do
-    desc "Control rule."
   end
 
   autorequire(:openldap_database) do
