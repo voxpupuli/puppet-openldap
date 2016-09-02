a34edc92640b6bc8c7a735ca8fec7d753a0156be
phamp008@gmail.com <phamp008@gmail.com>
2016-09-01 21:03:37 -0700

    fixing ident

--- spec/acceptance/openldap__server__overlay__spec.rb
+++ spec/acceptance/openldap__server__overlay__spec.rb
@@ -5,21 +5,21 @@ describe 'openldap::server::overlay' do
   context 'without parameters' do
     it 'creates an overlay' do
       pp = <<-EOS
-      $opnldap_overlay = { 'syncprov on dc=foo,dc=bar' => { 'ensure' => 'present', 'options' => { 'olcSpCheckpoint' => '100 10', 'olcSpSessionLog' => '100' }},  'ppolicy on dc=foo,dc=bar'  => { 'ensure' => 'present', 'options' => { 'olcPPolicyForwardUpdates' => 'FALSE', 'olcPPolicyUseLockout' => 'FALSE', 'olcPPolicyHashCleartext' => 'FALSE', 'olcPPolicyDefault' => 'cn=bojo,ou=policies,dc=foo,dc=bar'}}}
-      class { 'openldap::server': }
-      openldap::server::database { 'dc=foo,dc=bar':
-        ensure => present,
-      }
-      ->
-      openldap::server::module { 'syncprov':
-        ensure => present,
-      }
-      ->
-      openldap::server::module { 'ppolicy':
-        ensure => present,
-      }
-      ->
-      create_resources('::openldap::server::overlay', $opnldap_overlay)
+        $opnldap_overlay = { 'syncprov on dc=foo,dc=bar' => { 'ensure' => 'present', 'options' => { 'olcSpCheckpoint' => '100 10', 'olcSpSessionLog' => '100' }},  'ppolicy on dc=foo,dc=bar'  => { 'ensure' => 'present', 'options' => { 'olcPPolicyForwardUpdates' => 'FALSE', 'olcPPolicyUseLockout' => 'FALSE', 'olcPPolicyHashCleartext' => 'FALSE', 'olcPPolicyDefault' => 'cn=bojo,ou=policies,dc=foo,dc=bar'}}}
+        class { 'openldap::server': }
+        openldap::server::database { 'dc=foo,dc=bar':
+          ensure => present,
+        }
+        ->
+        openldap::server::module { 'syncprov':
+          ensure => present,
+        }
+        ->
+        openldap::server::module { 'ppolicy':
+          ensure => present,
+        }
+        ->
+        create_resources('::openldap::server::overlay', $opnldap_overlay)
       EOS
 
       apply_manifest(pp, :catch_failures => true)
