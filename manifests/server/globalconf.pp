b1984b53d9d7cc32c5a9b568f64ab01a70decd29
treydock <treydock@gmail.com>
2017-01-18 02:21:41 -0500

    Fix ordering so that Openldap::Server::Globalconf resources will come after the openldap service (#175)

--- manifests/server/globalconf.pp
+++ manifests/server/globalconf.pp
@@ -10,7 +10,12 @@ define openldap::server::globalconf(
 
   if $::openldap::server::provider == 'augeas' {
     Openldap::Server::Globalconf[$title] ~> Class['openldap::server::service']
+  } else {
+    Class['openldap::server::service'] ->
+    Openldap::Server::Globalconf[$title] ->
+    Class['openldap::server']
   }
+
   openldap_global_conf { $name:
     ensure   => $ensure,
     provider => $::openldap::server::provider,
