cc389b97a0bc395554897e4decd066a44cec250b
Tim Meusel <tim@bastelfreak.de>
2017-03-23 01:01:44 +0100

    drop legacy tests

--- spec/classes/openldap_client_config_spec.rb
+++ spec/classes/openldap_client_config_spec.rb
@@ -570,14 +570,6 @@ describe 'openldap::client::config' do
         end
       end
 
-      context 'with an invalid tls_cacert set' do
-        let :pre_condition do
-          "class {'openldap::client': tls_cacert => 'foo', }"
-        end
-
-        it { expect { is_expected.to compile }.to raise_error(/\"foo\" is not an absolute path/) }
-      end
-
       context 'with a valid tls_cacert set' do
         let :pre_condition do
           "class {'openldap::client': tls_cacert => '/etc/ssl/certs/ca-certificates.crt', }"
@@ -599,14 +591,6 @@ describe 'openldap::client::config' do
           })
           }
         end
-      end
-
-      context 'with an invalid tls_cacertdir set' do
-        let :pre_condition do
-          "class {'openldap::client': tls_cacertdir => 'foo', }"
-        end
-
-        it { expect { is_expected.to compile }.to raise_error(/\"foo\" is not an absolute path/) }
       end
 
       context 'with a valid tls_cacertdir set' do
--- spec/defines/openldap_server_database_spec.rb
+++ spec/defines/openldap_server_database_spec.rb
@@ -10,12 +10,6 @@ describe 'openldap::server::database' do
         facts
       end
 
-      context 'with an invalid directory' do
-        let(:params) {{ :directory => 'bar' }}
-
-        it { expect { is_expected.to compile }.to raise_error(/class ::openldap::server has not been evaluated/) }
-      end
-
       context 'without declaring Class[openldap::server]' do
         let(:params) {{ :directory => '/foo/bar' }}
 
