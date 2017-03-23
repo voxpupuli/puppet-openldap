e3359d21482b52a9711cea632b3d0ade74504d9f
Tim Meusel <tim@bastelfreak.de>
2017-03-23 01:00:26 +0100

    replace validate_* calles with datatypes
    the validate_* functions are deprecated since a few versions of stdlib
    and will be dropped in the near future. This PR replaces them with
    proper datatypes. The goal of this PR is to get rid of the function
    calls, not to implement datatypes to all parameters

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,29 +1,23 @@
 # See README.md for details.
 class openldap::server(
-  $package           = $openldap::params::server_package,
-  $confdir           = $openldap::params::server_confdir,
-  $conffile          = $openldap::params::server_conffile,
-  $service           = $openldap::params::server_service,
-  $service_hasstatus = $openldap::params::server_service_hasstatus,
-  $owner             = $openldap::params::server_owner,
-  $group             = $openldap::params::server_group,
-
-  $enable    = true,
-  $start     = true,
-
-  $provider  = 'olc',
-
-  $ssl_key   = undef,
-  $ssl_cert  = undef,
-  $ssl_ca    = undef,
-
-  $databases = {},
-
-  $ldap_ifs  = ['/'],
-  $ldaps_ifs = [],
-  $ldapi_ifs = ['/'],
+  $package                                 = $openldap::params::server_package,
+  $confdir                                 = $openldap::params::server_confdir,
+  $conffile                                = $openldap::params::server_conffile,
+  $service                                 = $openldap::params::server_service,
+  $service_hasstatus                       = $openldap::params::server_service_hasstatus,
+  $owner                                   = $openldap::params::server_owner,
+  $group                                   = $openldap::params::server_group,
+  $enable                                  = true,
+  $start                                   = true,
+  $provider                                = 'olc',
+  Optional[Stdlib::Absolutepath] $ssl_key  = undef,
+  Optional[Stdlib::Absolutepath] $ssl_cert = undef,
+  Optional[Stdlib::Absolutepath] $ssl_ca   = undef,
+  Hash $databases                          = {},
+  $ldap_ifs                                = ['/'],
+  $ldaps_ifs                               = [],
+  $ldapi_ifs                               = ['/'],
 ) inherits ::openldap::params {
-  validate_hash($databases)
 
   class { '::openldap::server::install': }
   -> class { '::openldap::server::config': }
