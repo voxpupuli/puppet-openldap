273cde33c700ff0b357e8ba6739760037c1693ff
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-21 15:25:38 -0400

    Rewrite openldap_password to use native Ruby
    The code is mostly copied from the openldap_database type's rootpw property
    The salt is generated using fqdn_rand_string to generate idempotent results

--- lib/puppet/parser/functions/openldap_password.rb
+++ lib/puppet/parser/functions/openldap_password.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module Puppet::Parser::Functions
   newfunction(:openldap_password, :type => :rvalue, :doc => <<-EOS
       Returns the openldap password hash from the clear text password.
@@ -7,10 +9,29 @@ module Puppet::Parser::Functions
     raise(Puppet::ParseError, "openldap_password(): Wrong number of arguments given") if args.size < 1 or args.size > 2
 
     secret = args[0]
-    command = ['slappasswd', '-s', secret]
-    scheme = args[1] if args[1]
-    command << ['-h', scheme] if scheme
+    scheme = args[1] || '{SSHA}'
 
-    Puppet::Util::Execution.execute(command.flatten).strip
+    Puppet::Parser::Functions.function('fqdn_rand_string')
+
+    case scheme[/([A-Z,0-9]+)/, 1]
+    when 'CRYPT'
+      salt = function_fqdn_rand_string([2])
+      password = '{CRYPT}' + secret.crypt(salt)
+    when 'MD5'
+      password = '{MD5}' + Digest::MD5.hexdigest(secret)
+    when 'SMD5'
+      salt = function_fqdn_rand_string([8])
+      md5_hash_with_salt = "#{Digest::MD5.digest(secret + salt)}#{salt}"
+      password = '{SMD5}' + [md5_hash_with_salt].pack('m').gsub("\n", '')
+    when 'SSHA'
+      salt = function_fqdn_rand_string([8])
+      password = '{SSHA}' + Base64.encode64("#{Digest::SHA1.digest(secret + salt)}#{salt}").chomp
+    when 'SHA'
+      password = '{SHA}' + Digest::SHA1.hexdigest(secret)
+    else
+      raise(Puppet::ParseError, "openldap_password(): Unrecognized scheme #{scheme}")
+    end
+
+    password
   end
 end
