67c60d3a7fb432e2dbcdf4e0dc8c0f16ded760f8
Raphaël Pinson <raphael.pinson@camptocamp.com>
2019-11-22 08:56:46 +0100

    Rubocop lint

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,8 +5,8 @@ require 'puppet_litmus'
 require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
 include PuppetLitmus
 
-def ldapsearch(cmd, exit_codes = [0,1], &block)
-  shell("ldapsearch #{cmd}", :acceptable_exit_codes => exit_codes, &block)
+def ldapsearch(cmd, exit_codes = [0, 1], &block)
+  shell("ldapsearch #{cmd}", acceptable_exit_codes: exit_codes, &block)
 end
 
 if ENV['TARGET_HOST'].nil? || ENV['TARGET_HOST'] == 'localhost'
