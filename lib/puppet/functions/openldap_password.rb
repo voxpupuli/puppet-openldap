b983adb1961eb8c9c4a5011f880212573ca7afed
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-02-04 17:39:17 +0100

    Port openldap_password() to Puppet 4.x function API
    Fixes #256

---
+++ lib/puppet/functions/openldap_password.rb
@@ -0,0 +1,48 @@
+require 'base64'
+#
+# @summary
+#         Returns the openldap password hash from the clear text password.
+#
+#
+Puppet::Functions.create_function(:openldap_password) do
+  # @param secret
+  #   The secret to be hashed.
+  #
+  # @param scheme
+  #   The optional scheme to use (defaults to SSHA).
+  #
+  # @return [String]
+  #   The hashed secret.
+  #
+  dispatch :generate_password do
+    required_param 'String', :secret
+    optional_param 'Enum["CRYPT","MD5","SMD5","SSHA","SHA"]', :scheme
+
+    return_type 'String'
+  end
+
+
+  def generate_password(secret, scheme='SSHA')
+    case scheme[%r{([A-Z,0-9]+)}, 1]
+    when 'CRYPT'
+      salt = call_function(:fqdn_rand_string, 2)
+      password = '{CRYPT}' + secret.crypt(salt)
+    when 'MD5'
+      password = '{MD5}' + Digest::MD5.hexdigest(secret)
+    when 'SMD5'
+      salt = call_function(:fqdn_rand_string, 8)
+      md5_hash_with_salt = "#{Digest::MD5.digest(secret + salt)}#{salt}"
+      password = '{SMD5}' + [md5_hash_with_salt].pack('m').gsub("\n", '')
+    when 'SSHA'
+      salt = call_function(:fqdn_rand_string, 8)
+      password = '{SSHA}' + Base64.encode64("#{Digest::SHA1.digest(secret + salt)}#{salt}").chomp
+    when 'SHA'
+      password = '{SHA}' + Digest::SHA1.hexdigest(secret)
+    else
+      raise(Puppet::ParseError, "openldap_password(): Unrecognized scheme #{scheme}")
+    end
+
+    password
+
+  end
+end
---
+++ spec/functions/openldap_password_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe :openldap_password do
+  it { is_expected.not_to eq(nil) }
+
+  it 'fails with wrong number of arguments' do
+    is_expected.to run.with_params.and_raise_error(StandardError)
+  end
+
+  it 'generates SSHA password with only a secret' do
+    expect(subject.func).to receive(:call_function).with(:fqdn_rand_string, 8).and_return('abcdefgh')
+    is_expected.to run.with_params('foo').and_return('{SSHA}3RXLE64s+3ytIRdJYu9eoU8O/alhYmNkZWZnaA==')
+  end
+
+  context 'when given a secret and a scheme' do
+    it 'generates CRYPT password' do
+      expect(scope).to receive(:function_fqdn_rand_string).with([2]).and_return('ab')
+      is_expected.to run.with_params('foo', 'CRYPT').and_return('{CRYPT}abQ9KY.KfrYrc')
+    end
+    it 'generates MD5 password' do
+      is_expected.to run.with_params('foo', 'MD5').and_return('{MD5}acbd18db4cc2f85cedef654fccc4a4d8')
+    end
+    it 'generates SMD5 password' do
+      expect(scope).to receive(:function_fqdn_rand_string).with([8]).and_return('abcdefgh')
+      is_expected.to run.with_params('foo', 'SMD5').and_return('{SMD5}NAYSvQYSIRYBLCM8U6MUc2FiY2RlZmdo')
+    end
+    it 'generates SSHA password' do
+      expect(scope).to receive(:function_fqdn_rand_string).with([8]).and_return('abcdefgh')
+      is_expected.to run.with_params('foo', 'SSHA').and_return('{SSHA}3RXLE64s+3ytIRdJYu9eoU8O/alhYmNkZWZnaA==')
+    end
+    it 'generates SHA password' do
+      is_expected.to run.with_params('foo', 'SHA').and_return('{SHA}0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33')
+    end
+  end
+end
