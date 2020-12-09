cf96ba0e865bf6a3692ed2c38cc243f042501258
Simon Hoenscheid <sh@example42.com>
2020-12-09 13:18:15 +0100

    * added Suse and SLES support * updated .travis.yml to use litmusimage repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,6 +17,11 @@ class openldap::params {
       }
       $utils_package            = 'ldap-utils'
       $escape_ldapi_ifs         = false
+      # added to fix spec tests
+      $enable_chown             = undef
+      $register_slp             = undef
+      $ldap_config_backend      = undef
+      $enable_memory_limit      = undef
     }
     'RedHat': {
       $client_package           = 'openldap'
@@ -33,6 +38,11 @@ class openldap::params {
       $server_service_hasstatus = true
       $utils_package            = 'openldap-clients'
       $escape_ldapi_ifs         = false
+      # added to fix spec tests
+      $enable_chown             = undef
+      $register_slp             = undef
+      $ldap_config_backend      = undef
+      $enable_memory_limit      = undef
     }
     'Archlinux': {
       $client_package           = 'openldap'
@@ -46,6 +56,11 @@ class openldap::params {
       $server_service_hasstatus = true
       $utils_package            = undef
       $escape_ldapi_ifs         = false
+      # added to fix spec tests
+      $enable_chown             = undef
+      $register_slp             = undef
+      $ldap_config_backend      = undef
+      $enable_memory_limit      = undef
     }
     'FreeBSD': {
       $client_package           = 'openldap-sasl-client'
@@ -59,6 +74,28 @@ class openldap::params {
       $server_service_hasstatus = true
       $utils_package            = undef
       $escape_ldapi_ifs         = true
+      # added to fix spec tests
+      $enable_chown             = undef
+      $register_slp             = undef
+      $ldap_config_backend      = undef
+      $enable_memory_limit      = undef
+    }
+    'Suse': {
+      $client_package           = 'openldap2-client'
+      $client_conffile          = '/etc/openldap/ldap.conf'
+      $server_confdir           = '/etc/openldap/slapd.d'
+      $server_conffile          = '/etc/openldap/slapd.conf'
+      $server_group             = 'ldap'
+      $server_owner             = 'ldap'
+      $server_package           = 'openldap2'
+      $server_service           = 'slapd'
+      $server_service_hasstatus = true
+      $utils_package            = undef
+      $escape_ldapi_ifs         = true
+      $enable_chown             = true
+      $register_slp             = true
+      $ldap_config_backend      = 'files'
+      $enable_memory_limit      = true
     }
     default: {
       fail "Operating System family ${::osfamily} not supported"
