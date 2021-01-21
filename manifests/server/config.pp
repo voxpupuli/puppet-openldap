f244d2feffe399d0759c3d47c8b12553f01531d8
Herve MARTIN <herve.martin@gmail.com>
2021-01-21 15:51:45 +0100

    Put cn=config ldif into a file

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -103,31 +103,7 @@ class openldap::server::config {
 
       if ($openldap::server::provider == 'olc') {
         # On FreeBSD we need to bootstrap slapd.d
-        $ldif = @(EOL)
-dn: cn=config
-objectClass: olcGlobal
-cn: config
-olcArgsFile: /var/run/openldap/slapd.args
-olcPidFile: /var/run/openldap/slapd.pid
-
-dn: olcDatabase={0}config,cn=config
-objectClass: olcDatabaseConfig
-olcDatabase: {0}config
-olcAccess: to dn.subtree="cn=config" by dn=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * none
-
-dn: cn=module,cn=config
-objectClass: olcModuleList
-cn: module
-olcModulepath:  /usr/local/libexec/openldap
-olcModuleload:  back_mdb.la
-olcModuleload:  back_ldap.la
-
-dn: cn=schema,cn=config
-objectClass: olcSchemaConfig
-cn: schema
-
-include: file:///usr/local/etc/openldap/schema/core.ldif
-EOL
+        $ldif = file('openldap/cn-config.ldif')
         exec { 'bootstrap cn=config':
           path    => '/usr/local/sbin',
           command => "echo '${ldif}' | slapadd -n 0 -F ${openldap::server::confdir}",
