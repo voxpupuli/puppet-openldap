0f8fb49e5ff0831f0208b7d9beb096d9d7c2f3a6
Romain Tartière <romain@blogreen.org>
2021-09-03 12:15:21 -1000

    Fix type of $openldap::server::access_wrapper::acl
    This wrapper defined type did not have tests and we unfortunately we
    broke it when we added data types.
    
    Add a basic unit test using the example in the README file.

---
+++ spec/defines/openldap_server_access_wrapper_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'openldap::server::access_wrapper' do
+  let(:title) { 'foo' }
+
+  let :pre_condition do
+    "class {'openldap::server':}"
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      let(:params) do
+        {
+          acl: {
+            '1 to *' => [
+              'by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage',
+              'by dn.exact=cn=admin,dc=example,dc=com write',
+              'by dn.exact=cn=replicator,dc=example,dc=com read',
+              'by * break',
+            ],
+            'to attrs=userPassword,shadowLastChange' => [
+              'by dn="cn=admin,dc=example,dc=com" write',
+              'by self write',
+              'by anonymous auth',
+            ],
+            '2 to *' => [
+              'by self read',
+            ],
+          }
+        }
+      end
+
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to have_openldap__server__access_resource_count(3) }
+    end
+  end
+end
