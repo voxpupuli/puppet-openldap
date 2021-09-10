8e7211eb038ee016f2beb0968d288b1b19e73d24
Romain Tartière <romain@blogreen.org>
2021-09-09 15:02:46 -1000

    Add some unit testing for the overlay provider

--- spec/unit/puppet/provider/openldap_overlay/olc_spec.rb
+++ spec/unit/puppet/provider/openldap_overlay/olc_spec.rb
@@ -19,14 +19,51 @@ describe Puppet::Type.type(:openldap_overlay).provider(:olc) do
     resource.provider
   end
 
+  let(:tmpfile) { instance_spy(Tempfile) }
+  let(:tmpfile_path) { double }
+  let(:tmpfile_content) { double }
+
   before do
     allow(provider).to receive(:slapcat).and_return('foo')
+    allow(Tempfile).to receive(:new).and_return(tmpfile)
+    allow(tmpfile).to receive(:path).and_return(tmpfile_path)
+    allow(IO).to receive(:read).with(tmpfile_path).and_return(tmpfile_content)
+    allow(Puppet).to receive(:debug).with(tmpfile_content)
+    allow(provider).to receive(:getDn).and_return('dc=example,dc=com')
+    allow(provider).to receive(:ldapmodify)
   end
 
-  describe 'when creating' do
-    it 'creates an overlay' do
-      pending 'needs work'
-      provider.create
+  describe '' do
+    describe 'when creating' do
+      it 'creates an overlay' do
+        provider.create
+        expect(tmpfile).to have_received(:<<).with("dn: olcOverlay=memberof,dc=example,dc=com\n")
+        expect(tmpfile).to have_received(:<<).with("objectClass: olcMemberOf\n")
+        expect(tmpfile).to have_received(:<<).with("olcOverlay: memberof\n")
+        expect(provider).to have_received(:ldapmodify)
+      end
+    end
+  end
+
+  describe '' do
+    let(:params) do
+      {
+        title: 'smbk5pwd on dc=example,dc=com',
+        options: {
+          'olcSmbK5PwdEnable' => %w[samba shadow],
+        },
+      }
+    end
+
+    describe 'when creating' do
+      it 'creates an overlay' do
+        provider.create
+        expect(tmpfile).to have_received(:<<).with("dn: olcOverlay=smbk5pwd,dc=example,dc=com\n")
+        expect(tmpfile).to have_received(:<<).with("objectClass: olcSmbK5PwdConfig\n")
+        expect(tmpfile).to have_received(:<<).with("olcOverlay: smbk5pwd\n")
+        expect(tmpfile).to have_received(:<<).with("olcSmbK5PwdEnable: samba\nolcSmbK5PwdEnable: shadow\n")
+        expect(provider).to have_received(:ldapmodify)
+      end
     end
   end
 end
