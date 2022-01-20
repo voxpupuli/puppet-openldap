dcee88199d32bcb0584bd555cefe60014c7517a7
Donatien ENEMAN <donatien.eneman@insee.fr>
2022-01-20 09:38:43 +0100

    (#345) :test_tube: add test to verify openldap_server_database_compile

--- spec/defines/openldap_server_database_spec.rb
+++ spec/defines/openldap_server_database_spec.rb
@@ -19,6 +19,39 @@ describe 'openldap::server::database' do
               is_expected.to contain_openldap__server__database('foo').with(directory: '/foo/bar')
             }
           end
+          context 'with all parameters set' do
+            let(:params) do
+              {
+                ensure: 'present',
+                relay: 'relay',
+                backend: 'ldap',
+                rootdn: 'cn=admin,dc=example,dc=com',
+                rootpw: 'secret',
+                initdb: true,
+                readonly: false,
+                sizelimit: '10000',
+                dbmaxsize: '10000',
+                timelimit: '10000',
+                updateref: 'default_updateref',
+                limits: [
+                  'dn.exact="cn=anyuser,dc=example,dc=org" size=100000',
+                  'dn.exact="cn=personnel,dc=example,dc=org" size=unlimited',
+                  'dn.exact="cn=dirsync,dc=example,dc=org" size=100000'
+                ],
+                dboptions: 'default_dboptions',
+                synctype: 'inclusive',
+                mirrormode: true,
+                syncusesubentry: 'wxw',
+                syncrepl: [
+                  'rid=1 provider=ldap://localhost searchbase="dc=foo,dc=example,dc=com"',
+                  'rid=2 provider=ldap://localhost searchbase="dc=foo,dc=example,dc=com"',
+                ],
+                security: 'olcSecurity: tls=128 '
+              }
+            end
+
+            it { is_expected.to compile.with_all_deps }
+          end
         end
       end
     end
