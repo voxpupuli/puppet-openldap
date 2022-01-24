69a73f4e3debab8e784874dfe63b65d2b60b0cf6
Romain Tartière <romain@blogreen.org>
2022-01-24 11:40:44 -1000

    Pet puppet-lint

--- manifests/server/database.pp
+++ manifests/server/database.pp
@@ -21,17 +21,20 @@ define openldap::server::database (
   Optional[Boolean]                             $mirrormode      = undef,
   Optional[String[1]]                           $syncusesubentry = undef,
   Optional[Variant[String[1],Array[String[1]]]] $syncrepl        = undef,
-  Hash[Enum[
-    'transport',
-    'sasl',
-    'simple_bind',
-    'ssf',
-    'tls',
-    'update_sasl',
-    'update_ssf',
-    'update_tls',
-    'update_transport',
-  ], Integer[0]]                                $security        = {},
+  Hash[
+    Enum[
+      'transport',
+      'sasl',
+      'simple_bind',
+      'ssf',
+      'tls',
+      'update_sasl',
+      'update_ssf',
+      'update_tls',
+      'update_transport',
+    ],
+    Integer[0]
+  ]                                             $security        = {},
 ) {
   include openldap::server
 
