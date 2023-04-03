53765dd17ffd4ccf84cc430703d6adb63a590400
Romain Tartière <romain@blogreen.org>
2023-04-03 12:07:00 -1000

    Add a test to dump and restore the LDAP directory
    This can spot some inconsistence issues where OpenLDAP is working
    correctly but a dump is not restorable, as spotted in #366.

--- spec/acceptance/db_spec.rb
+++ spec/acceptance/db_spec.rb
@@ -36,12 +36,15 @@ describe 'openldap::server::database' do
   end
 
   context 'without parameters' do
-    it 'creates a database' do
-      pp = <<-EOS
-      class { 'openldap::server': }
-      openldap::server::database { 'dc=foo,dc=com': }
+    let(:datadir) { '/var/lib/ldap' }
+    let(:pp) do
+      <<-EOS
+        class { 'openldap::server': }
+        openldap::server::database { 'dc=foo,dc=com': }
       EOS
+    end
 
+    it 'creates a database' do
       idempotent_apply(pp)
     end
 
@@ -49,6 +52,23 @@ describe 'openldap::server::database' do
       ldapsearch('-LLL -x -b dc=foo,dc=com') do |r|
         expect(r.stdout).to match(%r{dn: dc=foo,dc=com})
       end
+    end
+
+    it 'can dump the database' do
+      on default, 'slapcat -l /tmp/data.ldif'
+    end
+
+    it 'can restore the database' do
+      on default, puppet('resource service slapd ensure=stopped')
+      on default, "rm -r #{datadir}"
+      on default, "mkdir #{datadir}"
+      on default, 'slapadd -l /tmp/data.ldif'
+      on default, "chown -R ldap:ldap #{datadir}"
+      on default, puppet('resource service slapd ensure=running')
+    end
+
+    it 'has no change' do
+      apply_manifest(pp, catch_changes: true)
     end
   end
 
