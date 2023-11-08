b4fa44b6832753c615b93977dfeb14e439ab1ea2
Trey Dockendorf <tdockendorf@osc.edu>
2023-11-08 11:35:07 -0500

    Fix openldap_password function to avoid errors with latest Puppet 7
    Errors:
    
    Error: Could not retrieve catalog from remote server: Error 500 on SERVER: Server Error: Evaluation Error: Error while evaluating a Function Call, failed to coerce org.jruby.RubySymbol to java.lang.String
    
    Stack trace on puppetserver:
    
    /etc/puppetlabs/code/environments/test/modules/openldap/lib/puppet/functions/openldap_password.rb:38:in `generate_password'

--- spec/functions/openldap_password_spec.rb
+++ spec/functions/openldap_password_spec.rb
@@ -11,16 +11,16 @@ describe :openldap_password do
   end
 
   it 'generates SSHA password with only a secret' do
-    allow(subject.func).to receive(:call_function).with(:fqdn_rand_string, 8).and_return('abcdefgh')
+    allow(subject.func).to receive(:call_function).with('fqdn_rand_string', 8).and_return('abcdefgh')
     is_expected.to run.with_params('foo').and_return('{SSHA}3RXLE64s+3ytIRdJYu9eoU8O/alhYmNkZWZnaA==')
-    expect(subject.func).to have_received(:call_function).with(:fqdn_rand_string, 8)
+    expect(subject.func).to have_received(:call_function).with('fqdn_rand_string', 8)
   end
 
   context 'when given a secret and a scheme' do
     it 'generates CRYPT password' do
-      allow(subject.func).to receive(:call_function).with(:fqdn_rand_string, 2).and_return('ab')
+      allow(subject.func).to receive(:call_function).with('fqdn_rand_string', 2).and_return('ab')
       is_expected.to run.with_params('foo', 'CRYPT').and_return('{CRYPT}abQ9KY.KfrYrc')
-      expect(subject.func).to have_received(:call_function).with(:fqdn_rand_string, 2)
+      expect(subject.func).to have_received(:call_function).with('fqdn_rand_string', 2)
     end
 
     it 'generates MD5 password' do
@@ -28,15 +28,15 @@ describe :openldap_password do
     end
 
     it 'generates SMD5 password' do
-      allow(subject.func).to receive(:call_function).with(:fqdn_rand_string, 8).and_return('abcdefgh')
+      allow(subject.func).to receive(:call_function).with('fqdn_rand_string', 8).and_return('abcdefgh')
       is_expected.to run.with_params('foo', 'SMD5').and_return('{SMD5}NAYSvQYSIRYBLCM8U6MUc2FiY2RlZmdo')
-      expect(subject.func).to have_received(:call_function).with(:fqdn_rand_string, 8)
+      expect(subject.func).to have_received(:call_function).with('fqdn_rand_string', 8)
     end
 
     it 'generates SSHA password' do
-      allow(subject.func).to receive(:call_function).with(:fqdn_rand_string, 8).and_return('abcdefgh')
+      allow(subject.func).to receive(:call_function).with('fqdn_rand_string', 8).and_return('abcdefgh')
       is_expected.to run.with_params('foo', 'SSHA').and_return('{SSHA}3RXLE64s+3ytIRdJYu9eoU8O/alhYmNkZWZnaA==')
-      expect(subject.func).to have_received(:call_function).with(:fqdn_rand_string, 8)
+      expect(subject.func).to have_received(:call_function).with('fqdn_rand_string', 8)
     end
 
     it 'generates SHA password' do
