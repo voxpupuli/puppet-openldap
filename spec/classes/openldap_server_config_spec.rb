e6c2ad48773d0e76fccba4b7ec1335e1a4b6a3a7
Roberto Valentini <valantin89@gmail.com>
2024-03-25 12:37:11 +0100

    Add krb5 client keytab var Configure shell variable for kerberos configuration on debian family system

--- spec/classes/openldap_server_config_spec.rb
+++ spec/classes/openldap_server_config_spec.rb
@@ -17,5 +17,23 @@ describe 'openldap::server::config' do
         it { is_expected.not_to contain_openldap__globalconf('TLSCACertificateFile') }
       end
     end
+
+    next if facts[:osfamily] != 'Debian'
+
+    context "on #{os} with KRB5 conf" do
+      let(:facts) do
+        facts
+      end
+
+      let(:pre_condition) do
+        "class {'openldap::server': krb5_client_keytab_file => '/etc/krb5.keytab', }"
+      end
+
+      context 'with /etc/krb5.keytab' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('openldap::server::config') }
+        it { is_expected.to contain_shellvar('krb5_client_ktname').with(value: '/etc/krb5.keytab') }
+      end
+    end
   end
 end
