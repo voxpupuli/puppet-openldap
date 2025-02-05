4bdb46b5150a90c477f8b643f9c7cdb99d4349f1
Greg Cox <gcox@mozilla.com>
2025-02-05 18:45:11 +0000

    Add chain overlay

--- spec/unit/puppet/provider/openldap_overlay/olc_spec.rb
+++ spec/unit/puppet/provider/openldap_overlay/olc_spec.rb
@@ -56,6 +56,48 @@ describe Puppet::Type.type(:openldap_overlay).provider(:olc) do
         )
       end
     end
+
+    describe 'chain' do
+      before do
+        slapcat_overlay_output = <<~OUTPUT
+          dn: olcOverlay={0}chain,olcDatabase={-1}frontend,cn=config
+          objectClass: olcConfig
+          objectClass: olcOverlayConfig
+          objectClass: olcChainConfig
+          olcOverlay: {0}chain
+          olcChainCacheURI: FALSE
+          olcChainMaxReferralDepth: 1
+          olcChainReturnError: TRUE
+        OUTPUT
+        slapcat_db_output = <<~OUTPUT
+          dn: olcDatabase={-1}frontend,cn=config
+          objectClass: olcDatabaseConfig
+          objectClass: olcFrontendConfig
+          olcDatabase: {-1}frontend
+        OUTPUT
+        allow(described_class).to receive(:slapcat).with(
+          '(olcOverlay=*)'
+        ).and_return(slapcat_overlay_output)
+        allow(described_class).to receive(:slapcat).with(
+          '(olcDatabase={-1}frontend)'
+        ).and_return(slapcat_db_output)
+      end
+
+      it 'reads a chain object' do
+        expect(described_class.instances.size).to eq(1)
+        expect(described_class.instances[0].name).to eq('chain on cn=frontend')
+        expect(described_class.instances[0].overlay).to eq('chain')
+        expect(described_class.instances[0].suffix).to eq('cn=frontend')
+        expect(described_class.instances[0].index).to eq(0)
+        expect(described_class.instances[0].options).to eq(
+          {
+            'olcChainCacheURI'         => 'FALSE',
+            'olcChainMaxReferralDepth' => '1',
+            'olcChainReturnError'      => 'TRUE',
+          }
+        )
+      end
+    end
   end
 
   describe 'creating overlay' do
@@ -86,11 +128,14 @@ describe Puppet::Type.type(:openldap_overlay).provider(:olc) do
       allow(tmpfile).to receive(:path).and_return(tmpfile_path)
       allow(IO).to receive(:read).with(tmpfile_path).and_return(tmpfile_content)
       allow(Puppet).to receive(:debug).with(tmpfile_content)
-      allow(provider).to receive(:getDn).and_return('dc=example,dc=com')
       allow(provider).to receive(:ldapmodify)
     end
 
     describe 'when creating' do
+      before do
+        allow(provider).to receive(:getDn).and_return('dc=example,dc=com')
+      end
+
       it 'creates an overlay' do
         provider.create
         expect(tmpfile).to have_received(:<<).with("dn: olcOverlay=memberof,dc=example,dc=com\n")
@@ -101,6 +146,10 @@ describe Puppet::Type.type(:openldap_overlay).provider(:olc) do
     end
 
     describe 'with smbk5pwd' do
+      before do
+        allow(provider).to receive(:getDn).and_return('dc=example,dc=com')
+      end
+
       let(:params) do
         {
           title: 'smbk5pwd on dc=example,dc=com',
@@ -117,6 +166,29 @@ describe Puppet::Type.type(:openldap_overlay).provider(:olc) do
           expect(tmpfile).to have_received(:<<).with("objectClass: olcSmbK5PwdConfig\n")
           expect(tmpfile).to have_received(:<<).with("olcOverlay: smbk5pwd\n")
           expect(tmpfile).to have_received(:<<).with("olcSmbK5PwdEnable: samba\nolcSmbK5PwdEnable: shadow\n")
+          expect(provider).to have_received(:ldapmodify)
+        end
+      end
+    end
+
+    describe 'with chain' do
+      let(:params) do
+        {
+          title: 'chain on cn=frontend',
+          suffix: 'cn=frontend',
+          options: {
+            'olcChainMaxReferralDepth' => '1',
+          },
+        }
+      end
+
+      describe 'when creating' do
+        it 'creates an overlay' do
+          provider.create
+          expect(tmpfile).to have_received(:<<).with("dn: olcOverlay=chain,olcDatabase={-1}frontend,cn=config\n")
+          expect(tmpfile).to have_received(:<<).with("objectClass: olcChainConfig\n")
+          expect(tmpfile).to have_received(:<<).with("olcOverlay: chain\n")
+          expect(tmpfile).to have_received(:<<).with("olcChainMaxReferralDepth: 1\n")
           expect(provider).to have_received(:ldapmodify)
         end
       end
